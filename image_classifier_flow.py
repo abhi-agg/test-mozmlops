ba978e0a1ddd6de98d407c5f17d02756a61df61c
Abhishek Aggarwal <aaggarwal@mozilla.com>
2024-09-30 14:55:27 +0200

    Refactoring
     - Separate python script for model definition

--- image_classifier_flow.py
+++ image_classifier_flow.py
@@ -1,7 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 import os
 from metaflow import (
     FlowSpec,
-    IncludeFile,
     Parameter,
     card,
     current,
@@ -13,12 +16,14 @@ from metaflow import (
 )
 from metaflow.cards import Markdown
 
-GCS_PROJECT_NAME = "moz-fx-mlops-inference-nonprod"
-GCS_BUCKET_NAME = "mf-models-test1"
-MODEL_STORAGE_PATH="abhishek-mlops-hackdays/model-bytes.pth"
+# Set the right GCP project and GCS bucket
+GCS_PROJECT_NAME = "your-gcp-project-here"
+GCS_BUCKET_NAME = "your-gcs-bucket-here"
+# Model blob to be uploaded to GCS
+MODEL_STORAGE_PATH = "image_classifier/trained-model-bytes.pth"
 
-class ImageClassifier(FlowSpec):
 
+class ImageClassifierFlow(FlowSpec):
     # This is an example of a parameter. You can toggle this when you call the flow
     # with python template_flow.py run --offline False
     offline_wandb = Parameter(
@@ -28,8 +33,7 @@ class ImageClassifier(FlowSpec):
         default=True,
     )
 
-
-    @pypi(python='3.11.9', packages={'torchvision': '0.19.1'})
+    @pypi(python="3.11.9", packages={"torchvision": "0.19.1"})
     @card(type="default")
     @kubernetes
     @step
@@ -38,24 +42,30 @@ class ImageClassifier(FlowSpec):
         import torchvision.transforms as transforms
 
         # Download and normalize CIFAR10
-        print(f'start step: downloading and normalizing dataset')
+        print("start step: downloading and normalizing dataset")
         transform = transforms.Compose(
-            [transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
         )
-        batch_size = 4
-        self.trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                                download=True, transform=transform)
-        self.testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                            download=True, transform=transform)
-        self.next(self.train)
 
+        self.trainset = torchvision.datasets.CIFAR10(
+            root="./data", train=True, download=True, transform=transform
+        )
+        self.testset = torchvision.datasets.CIFAR10(
+            root="./data", train=False, download=True, transform=transform
+        )
+        self.next(self.train)
 
     # Train the network
     # Keep @nvidia decorator before @step decorator else the flow fails
-    @pypi(python='3.11.9', packages={'torch': '2.4.1', 'torchvision': '0.19.1', 'mozmlops': '0.1.4'},)
-    @nvidia
-    #@kubernetes
+    @pypi(
+        python="3.11.9",
+        packages={"torch": "2.4.1", "torchvision": "0.19.1", "mozmlops": "0.1.4"},
+    )
+    # @nvidia
+    @kubernetes
     @card
     @environment(
         vars={
@@ -67,8 +77,8 @@ class ImageClassifier(FlowSpec):
     def train(self):
         import torch
         import torch.nn as nn
-        import torch.nn.functional as F
         import torch.optim as optim
+        from image_classifier_model import ImageClassifierModel
         from io import BytesIO
         import wandb
         import os
@@ -85,43 +95,27 @@ class ImageClassifier(FlowSpec):
         # Check if GPU is available
         if torch.cuda.is_available():
             import os
+
             print(os.system("nvidia-smi"))
             device = torch.device("cuda")
 
         print(f"Training on: {device}")
 
-        # Define a Convolutional Neural Network
-        class Net(nn.Module):
-            def __init__(self):
-                super().__init__()
-                self.conv1 = nn.Conv2d(3, 6, 5)
-                self.pool = nn.MaxPool2d(2, 2)
-                self.conv2 = nn.Conv2d(6, 16, 5)
-                self.fc1 = nn.Linear(16 * 5 * 5, 120)
-                self.fc2 = nn.Linear(120, 84)
-                self.fc3 = nn.Linear(84, 10)
-
-            def forward(self, x):
-                x = self.pool(F.relu(self.conv1(x)))
-                x = self.pool(F.relu(self.conv2(x)))
-                x = torch.flatten(x, 1) # flatten all dimensions except batch
-                x = F.relu(self.fc1(x))
-                x = F.relu(self.fc2(x))
-                x = self.fc3(x)
-                return x
-
-        net = Net().to(device)
+        image_classifier_model = ImageClassifierModel().to(device)
 
         # Define a Loss function and optimizer
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+        optimizer = optim.SGD(
+            image_classifier_model.parameters(), lr=0.001, momentum=0.9
+        )
 
-        # load train data
+        # Load train data
         batch_size = 4
-        trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=batch_size,
-                                                shuffle=True, num_workers=2)
+        trainloader = torch.utils.data.DataLoader(
+            self.trainset, batch_size=batch_size, shuffle=True, num_workers=2
+        )
 
-        # start training
+        # Start training
         num_epochs = 2
         for epoch in range(num_epochs):  # loop over the dataset multiple times
             running_loss = 0.0
@@ -133,99 +127,88 @@ class ImageClassifier(FlowSpec):
                 optimizer.zero_grad()
 
                 # forward + backward + optimize
-                outputs = net(inputs)
+                outputs = image_classifier_model(inputs)
                 loss = criterion(outputs, labels)
                 loss.backward()
                 optimizer.step()
 
                 # print statistics
                 running_loss += loss.item()
-                if i % 2000 == 1999:    # print every 2000 mini-batches
-                    print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+                if i % 2000 == 1999:  # print every 2000 mini-batches
+                    print(f"[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}")
                     # log metrics to wandb
                     wandb.log({"mini-batches": {i + 1}, "loss": {running_loss / 2000}})
                     running_loss = 0.0
 
-        print('Finished Training')
+        print("Finished Training")
         buffer = BytesIO()
-        torch.save(net.state_dict(), buffer)
+        torch.save(image_classifier_model.state_dict(), buffer)
         self.model_state_dict_bytes = buffer.getvalue()
         self.next(self.evaluate)
 
-
-    # Test the network on the test data
-    @pypi(python='3.11.9', packages={'torch': '2.4.1', 'torchvision': '0.19.1',})
+    # Test the model on the test data
+    @pypi(
+        python="3.11.9",
+        packages={
+            "torch": "2.4.1",
+            "torchvision": "0.19.1",
+        },
+    )
     @kubernetes
     @step
     def evaluate(self):
         import torch
-        import torch.nn as nn
-        import torch.nn.functional as F
+        from image_classifier_model import ImageClassifierModel
         from io import BytesIO
 
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print(f"Evaluating on: {device}")
 
-        # Define a Convolutional Neural Network
-        class Net(nn.Module):
-            def __init__(self):
-                super().__init__()
-                self.conv1 = nn.Conv2d(3, 6, 5)
-                self.pool = nn.MaxPool2d(2, 2)
-                self.conv2 = nn.Conv2d(6, 16, 5)
-                self.fc1 = nn.Linear(16 * 5 * 5, 120)
-                self.fc2 = nn.Linear(120, 84)
-                self.fc3 = nn.Linear(84, 10)
-
-            def forward(self, x):
-                x = self.pool(F.relu(self.conv1(x)))
-                x = self.pool(F.relu(self.conv2(x)))
-                x = torch.flatten(x, 1) # flatten all dimensions except batch
-                x = F.relu(self.fc1(x))
-                x = F.relu(self.fc2(x))
-                x = self.fc3(x)
-                return x
-
-        net = Net().to(device)
+        image_classifier_model = ImageClassifierModel().to(device)
         buffer = BytesIO(self.model_state_dict_bytes)
-        net.load_state_dict(torch.load(buffer, map_location=device, weights_only=True))
+        image_classifier_model.load_state_dict(
+            torch.load(buffer, map_location=device, weights_only=True)
+        )
 
         correct = 0
         total = 0
 
         # load test data
         batch_size = 4
-        testloader = torch.utils.data.DataLoader(self.testset, batch_size=batch_size,
-                                                shuffle=False, num_workers=2)
+        testloader = torch.utils.data.DataLoader(
+            self.testset, batch_size=batch_size, shuffle=False, num_workers=2
+        )
         # since we're not training, we don't need to calculate the gradients for our outputs
         with torch.no_grad():
             for data in testloader:
                 images, labels = data[0].to(device), data[1].to(device)
                 # calculate outputs by running images through the network
-                outputs = net(images)
+                outputs = image_classifier_model(images)
                 # the class with the highest energy is what we choose as prediction
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
-        print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+        print(
+            f"Accuracy of the network on the 10000 test images: {100 * correct // total} %"
+        )
         self.next(self.upload_model_to_gcs)
 
-
-    @pypi(python='3.11.9', packages={'mozmlops': '0.1.4'})
+    @pypi(python="3.11.9", packages={"mozmlops": "0.1.4"})
     @kubernetes
     @step
     def upload_model_to_gcs(self):
         from mozmlops.cloud_storage_api_client import CloudStorageAPIClient
 
-        print(f"Uploading model to gcs")
+        print("Uploading model to gcs")
         # init client
         storage_client = CloudStorageAPIClient(
             project_name=GCS_PROJECT_NAME, bucket_name=GCS_BUCKET_NAME
         )
-        storage_client.store(data=self.model_state_dict_bytes, storage_path=MODEL_STORAGE_PATH)
+        storage_client.store(
+            data=self.model_state_dict_bytes, storage_path=MODEL_STORAGE_PATH
+        )
         self.next(self.end)
-
 
     @kubernetes
     @step
@@ -238,5 +221,6 @@ class ImageClassifier(FlowSpec):
             """
         )
 
+
 if __name__ == "__main__":
-    ImageClassifier()
+    ImageClassifierFlow()
